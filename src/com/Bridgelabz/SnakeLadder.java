0c4d9fea2eec82b0250fff84271f8aecced0571b
shivraj2806 <shivjadhav2806@gmail.com>
2021-08-12 19:17:40 +0530

    Added:Posion_of_the_player

--- src/com/Bridgelabz/SnakeLadder.java
+++ src/com/Bridgelabz/SnakeLadder.java
@@ -3,7 +3,8 @@ package com.Bridgelabz;
 public class SnakeLadder {
     public static void main(String[] args) {
         System.out.println("Welcome To Snake and Ladder Game");
-        double randomnum=(int)Math.floor(Math.random()*10)%6;
-        System.out.println("getting random number: "+randomnum);
+        int start_Potion=0;
+        System.out.println("single player started at position 0"+start_Potion);
+
     }
 }
