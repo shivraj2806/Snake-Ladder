43a5f3a5beff046b6d02816506a849f990fcc924
shivraj2806 <shivjadhav2806@gmail.com>
2021-08-12 18:51:47 +0530

    Added:getting_random_number1to6

--- src/com/Bridgelabz/SnakeLadder.java
+++ src/com/Bridgelabz/SnakeLadder.java
@@ -3,5 +3,7 @@ package com.Bridgelabz;
 public class SnakeLadder {
     public static void main(String[] args) {
         System.out.println("Welcome To Snake and Ladder Game");
+        double randomnum=(int)Math.floor(Math.random()*10)%6;
+        System.out.println("getting random number: "+randomnum);
     }
 }
