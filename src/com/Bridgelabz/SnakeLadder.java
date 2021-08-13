b1e781f9f82c79c25fbc4b647007cb849b731b0d
shivraj2806 <shivjadhav2806@gmail.com>
2021-08-13 21:42:10 +0530

    SnakeLadder.java

--- src/com/Bridgelabz/SnakeLadder.java
+++ src/com/Bridgelabz/SnakeLadder.java
@@ -1,6 +1,7 @@
 package com.Bridgelabz;
 
-public class SnakeLadder{
+public class SnakeLadder {
+
     public static final int WINNING_POSITION = 100;
     public static int PLAYER_POSITION = 0;
 
@@ -10,24 +11,25 @@ public class SnakeLadder{
         int noPlay = 3;
         System.out.println("Welcome to the Snake and Ladder Game");
 
-
-        // Using while loop to repeat the game till player reaches exact winning
-        // position Using Math.random to generate random numbers between 1 to 6
-
+        /**
+         * Using while loop to repeat the game till player reaches exact winning
+         * position Using Math.random to generate random numbers between 1 to 6
+         */
         while (PLAYER_POSITION < WINNING_POSITION) {
             int Dice = (int) (Math.random() * 6 + 1);
             System.out.println("The Player's die number is : " + Dice);
 
-
-            //Using Math.random() function to generate random between 1 to 3 for user to
-            //get options
-
+            /**
+             * Using Math.random() function to generate random between 1 to 3 for user to
+             * get options
+             */
             int playerCheck = (int) Math.floor(Math.random() * 3 + 1);
-
             switch (playerCheck) {
                 case 1:
-                    PLAYER_POSITION = PLAYER_POSITION + Dice;
-                    System.out.println("The player position after ladder is :" + PLAYER_POSITION);
+                    if ((PLAYER_POSITION + Dice) > 100)
+                        PLAYER_POSITION = PLAYER_POSITION;
+                    else
+                        PLAYER_POSITION = PLAYER_POSITION + Dice;
                     break;
                 case 2:
                     PLAYER_POSITION = PLAYER_POSITION - Dice;
@@ -37,16 +39,16 @@ public class SnakeLadder{
                     PLAYER_POSITION = 0;
                     System.out.println("There is no play and the player position is :" + PLAYER_POSITION);
 
-
-                    // The player position will be set to zero if the player goes below zero
-
+                    /**
+                     * The player position will be set to zero if the player goes below zero
+                     */
 
                     if (PLAYER_POSITION < 0) {
                         System.out.println("You need to start again from starting position");
                         PLAYER_POSITION = 0;
                     }
             }
-
         }
     }
+
 }
